2ee0b3c02fb0b7a64449d93576a23a24e994e11e
Brandon Haupt <haupt.brandon@gmail.com>
2023-03-21 18:07:40 -0500

    updated stuff

--- naturesgarden/serializers.py
+++ naturesgarden/serializers.py
@@ -2,5 +2,6 @@ from .models import NaturesGarden
 from rest_framework import serializers
 
 class NatureSerializer(serializers.HyperlinkedModelSerializer):
-    model = NaturesGarden
-    fields = ['id', 'url', 'name', "edible", "origin", "benefits", "description"]
+    class Meta:
+        model = NaturesGarden
+        fields = ['id', 'url', 'name', "edible", "origin", "benefits", "description"]
