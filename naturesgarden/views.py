4f25b8dd7ec2298550f76a5a6ed6db2882ad80c3
Brandon Haupt <haupt.brandon@gmail.com>
2023-03-21 17:34:03 -0500

    updated stuff

--- naturesgarden/views.py
+++ naturesgarden/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import NaturesGarden
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import NatureSerializer
 
 # Create your views here.
+class NaturegardenViewSet(viewsets.ModelViewSet):
+    queryset = NaturesGarden.objects.all()
+    
+    serializer_class = NatureSerializer
+    
+    permission_classes = [permissions.AllowAny]
